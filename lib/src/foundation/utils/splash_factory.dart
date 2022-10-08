fbdf07afaefccfb835c30e75483c240d83f98758
Arne Molland <arne@molland.sh>
2022-10-08 13:14:49 +0200

    foundation/utils: implement NoSplashFactory

---
+++ lib/src/foundation/utils/splash_factory.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// {@category Core}
+/// Splash factory that disables all ripple effects
+class NoSplashFactory extends InteractiveInkFeatureFactory {
+  const NoSplashFactory();
+
+  @override
+  InteractiveInkFeature create({
+    required MaterialInkController controller,
+    required RenderBox referenceBox,
+    required Offset position,
+    required Color color,
+    required TextDirection textDirection,
+    bool containedInkWell = false,
+    RectCallback? rectCallback,
+    BorderRadius? borderRadius,
+    ShapeBorder? customBorder,
+    double? radius,
+    VoidCallback? onRemoved,
+  }) {
+    return NoSplash(
+      controller: controller,
+      referenceBox: referenceBox,
+      color: color,
+    );
+  }
+}
+
+/// A splash effect that disables all ripple effects
+class NoSplash extends InteractiveInkFeature {
+  NoSplash({
+    required MaterialInkController controller,
+    required RenderBox referenceBox,
+    required Color color,
+  }) : super(
+          controller: controller,
+          referenceBox: referenceBox,
+          color: color,
+        );
+
+  @override
+  void paintFeature(Canvas canvas, Matrix4 transform) {}
+}
