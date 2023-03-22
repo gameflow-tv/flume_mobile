b1ab9a58885c7631be5abf1b3c68b475bbbba511
Arne Molland <arne@molland.sh>
2023-03-22 16:02:56 +0100

    material: hide SearchBar export

--- lib/material.dart
+++ lib/material.dart
@@ -1,2 +1,2 @@
 /// Re-export the Material package with some symbols hidden to avoid conflicts.
-export 'package:flutter/material.dart' hide Checkbox, IconButton;
+export 'package:flutter/material.dart' hide Checkbox, IconButton, SearchBar;
